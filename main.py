75631ff9e754ea581ca2059ff3997bf6823a5f37
prgrm274 <programmer2704878@gmail.com>
2026-03-11 15:42:21 +0700

    feat: connect db with FastAPI

---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Depends
+from strawberry.fastapi import GraphQLRouter
+import strawberry
+from app.graphql.queries import Query
+from app.graphql.mutations import Mutation
+from app.database import init_db, get_db
+
+# Init db table when app starts
+init_db()
+
+# Combine Query (and Mutation later) in one Schema
+schema = strawberry.Schema(query=Query, mutation=Mutation)
+
+# To insert db session into GraphQL context
+async def get_context(db=Depends(get_db)):
+    return {"db": db}
+
+# GraphQL router
+graphql_app = GraphQLRouter(schema, context_getter=get_context, multipart_uploads_enabled=True)
+
+app = FastAPI(
+    title="Inventory API",
+    description="Python FastAPI Backend for Inventory with React frontend"
+)
+
+# Attach the route on endpoint "/graphql"
+app.include_router(graphql_app, prefix="/graphql")
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Inventory API. Go to /graphql to explore."}
